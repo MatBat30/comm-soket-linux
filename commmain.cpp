0bcbc590afd78a615a5eb8ac76f6427aa99eafa8
Shura <mathisbatailler30@gmail.com>
2023-04-12 12:16:53 +0200

    Update commmain.cpp

--- commmain.cpp
+++ commmain.cpp
@@ -11,32 +11,32 @@ constexpr int PORT = 8080;
 constexpr int BUFFER_SIZE = 4096;
 
 int main() {
-    // Crée un socket
+    // Cr�e un socket
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket < 0) {
-        std::cerr << "Erreur de création du socket" << std::endl;
+        std::cerr << "Erreur de cr�ation du socket" << std::endl;
         return 1;
     }
 
-    // Prépare l'adresse du serveur
+    // Pr�pare l'adresse du serveur
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(PORT);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    // Lie le socket à l'adresse
+    // Lie le socket � l'adresse
     if (bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) < 0) {
         std::cerr << "Erreur de liaison du socket" << std::endl;
         return 1;
     }
 
-    // Écoute les connexions entrantes
+    // �coute les connexions entrantes
     if (listen(serverSocket, 1) < 0) {
-        std::cerr << "Erreur d'écoute" << std::endl;
+        std::cerr << "Erreur d'�coute" << std::endl;
         return 1;
     }
 
-    std::cout << "Serveur en écoute sur le port " << PORT << std::endl;
+    std::cout << "Serveur en �coute sur le port " << PORT << std::endl;
 
     // Accepte une connexion entrante
     sockaddr_in clientAddress;
@@ -48,9 +48,9 @@ int main() {
         return 1;
     }
 
-    std::cout << "Connexion acceptée" << std::endl;
+    std::cout << "Connexion accept�e" << std::endl;
 
-    // Reçoit les données du client et les stocke dans un vecteur
+    // Re�oit les donn�es du client et les stocke dans un vecteur
     std::vector<char> receivedData;
     char buffer[BUFFER_SIZE];
     ssize_t bytesRead;
@@ -59,18 +59,24 @@ int main() {
         receivedData.insert(receivedData.end(), buffer, buffer + bytesRead);
     }
 
-    std::cout << "Données reçues (" << receivedData.size() << " octets)" << std::endl;
+    std::cout << "Donn�es re�ues (" << receivedData.size() << " octets)" << std::endl;
+
+    // Convertit les donn�es en big-endian
+    for (size_t i = 0; i < receivedData.size(); i += 4) {
+        uint32_t* dataPtr = reinterpret_cast<uint32_t*>(&receivedData[i]);
+        *dataPtr = htonl(*dataPtr);
+    }
 
     // Ferme les sockets
     close(clientSocket);
     close(serverSocket);
 
-    // Écrit les données reçues dans un fichier
+    // �crit les donn�es re�ues dans un fichier
     std::ofstream outputFile("output.jpg", std::ios::binary);
     outputFile.write(receivedData.data(), receivedData.size());
     outputFile.close();
 
-    std::cout << "Données écrites dans output.jpg" << std::endl;
+    std::cout << "Donn�es �crites dans output.jpg" << std::endl;
 
     return 0;
 }
